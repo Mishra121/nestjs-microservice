7e5a8a246e5bdf57b9e3bea70223d993b157ee74
Z0086L0 <Vibhu.Mishra@target.com>
2022-05-22 14:28:45 +0530

    product admin crud api added

---
+++ admin/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345678',
+      database: 'nest_admin',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
